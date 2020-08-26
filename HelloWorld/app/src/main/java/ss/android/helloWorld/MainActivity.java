f3b24b09e61dba68e3c9016e497f174f8f7205c6
Andro-ide <shiva.aide.creativity@gmail.com>
2020-08-26 16:26:14 +0530

    Hello World

---
+++ HelloWorld/app/src/main/java/ss/android/helloWorld/MainActivity.java
@@ -0,0 +1,17 @@
+package ss.android.helloWorld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+// Declare Components Between "{" and "@" to auto import.
+
+public class MainActivity extends AppCompatActivity {
+    // Declare Component here
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        // Link XML to Java using 👉 findViewById(int id) 👈 method.
+        // Code Here...
+    }
+}
